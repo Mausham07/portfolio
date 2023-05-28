2956059dda385ab868f9a505668964d38b4a5dbd
Mausham Bista <maushambista0@gmail.com>
2023-05-28 03:46:34 -0500

    edited few

--- src/components/footer/Footer.jsx
+++ src/components/footer/Footer.jsx
@@ -1,13 +1,25 @@
 import React from 'react';
 import './footer.css';
+import photo from '../../img/linkedin.png'
+import photo1 from '../../img/github.png'
 
 const Footer = () => {
   return (
     <footer className="footer">
-      <div className="footer-content">
-        <p className="footer-text">
-          &copy; {new Date().getFullYear()} Mausham Bista. All rights reserved.
-        </p>
+      <div className="foot">
+        <div className="footer-content">
+          <p className="footer-text">
+            &copy; {new Date().getFullYear()} Mausham Bista. All rights reserved.
+          </p>
+        </div>
+        <div className="social">
+          <a href="https://www.linkedin.com/in/mausham-bista/">
+            <img src={photo} alt="" className='link'/>
+          </a>
+          <a href="https://github.com/Mausham07">
+            <img src={photo1} alt="" />
+          </a>
+        </div>
       </div>
     </footer>
   );
